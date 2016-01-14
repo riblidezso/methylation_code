e99b7eaed6ebf6cf942823de5145d7b0d0fabe35
riblidezso <dkribli@gmail.com>
2016-01-15 00:47:35 +0100

    started codebase, changed naive models

--- my_modules/sqlite_utils.py
+++ my_modules/sqlite_utils.py
@@ -1,5 +1,16 @@
+import time
+import subprocess
+
 def run_sqlilte3(command,db,output=''):
-	"""Execute sql query on database."""
+	"""
+	Execute sql query on database.
+
+	Output written to filename if given.
+	Note: Using the sqlite3 command line tool with a temp file.
+		This way the query can be interrupted without killing the python kernel.
+	Note: /usr/bin/sqlite3 is 3-4 times faster than anaconda sqlite3.
+
+	"""
 	start=time.time()
 	with open('tempf.sql','w') as tempf:
 		tempf.write(command)
